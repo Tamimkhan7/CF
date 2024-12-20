e66b97a59ba73ca8ab28ebcaa7b206600c94f5fc
Tamim khan <mdtamimkhan842@gmail.com>
2024-12-20 20:47:29 +0600

    modified dv 3 and mutithreading and Beautiful sets of points

---
+++ Random/B. Multithreading.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MTK                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define mem(a, b) memset(a, b, sizeof(a))
+#define show(x) cout << #x << ' ' << x << endl
+#define all(x) (x).begin(), (x).end()
+#define ll int long long
+#define mod 1000000007
+
+int32_t main()
+{
+    MTK;
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+        cin >> v[i];
+    // reverse(all(v));
+    int i = n - 1;
+    while (i > 0 and v[i - 1] < v[i])
+        i--;
+    cout << i << '\n';
+    return 0;
+}
